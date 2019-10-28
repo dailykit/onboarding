750e0306bc4f04d74f214ff7bb7cfc530b73b6c7
Suresh <katikalasureshkumar@gmail.com>
2019-10-29 02:32:05 +0530

    Base template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from './logo.svg';
+import logo from "./assets/image.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="left-panel">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <p className="company"> Tell us about your company</p>
+          <p className="company-caption">
+            {" "}
+            so we prepare everything as per your needs{" "}
+          </p>
+
+          <p className="host">Host your Dailykit </p>
+          <p className="host-caption">
+            {" "}
+            with us or self-host and invite your team
+          </p>
+        </div>
+
+        <div className="right-panel">
+          <p className="main-text">
+            Set up your account with us in 2 simple steps
+          </p>
+          <p className="caption">Lets get started</p>
+          <div className="button">
+            <span className="button-text">LETS BEGIN</span>
+          </div>
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
+// <body>
+
+// </body>
+// <footer></footer>
+
+//           <div className="outer-circle"> </div>
