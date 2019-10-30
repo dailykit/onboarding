e236f0527c946f9b8d6221d0df0412dd41ffbf4c
Suresh <katikalasureshkumar@gmail.com>
2019-10-30 16:16:37 +0530

    Added : basic design for signup components

--- src/pages/BasicInfo.js
+++ src/pages/BasicInfo.js
@@ -6,10 +6,44 @@ const BasicInfo = ({ match }) => {
   console.log(match);
   return (
     <div>
-      <img className="icon" src={logo} alt="logo" />
-      <div className="line"> </div>
+      <table className="icon-table">
+        <tbody>
+          <tr>
+            <th>
+              <img className="icon" src={logo} alt="logo" />
+            </th>
+            <th>
+              <div className="line"> </div>
+            </th>
+
+            <th>
+              <div className="heading">Basic Information</div>
+            </th>
+          </tr>
+        </tbody>
+      </table>
+
+      <div className="box">
+        <table className="inner-table">
+          <tbody>
+            <tr>Register with your work address</tr>
+            <tr>
+              {" "}
+              <input type="text" name="workemail" placeholder="work email" />
+            </tr>
+            <tr>
+              <input type="text" name="password" placeholder="password" />
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
 
 export default BasicInfo;
+
+// <div>
+//
+//
+// </div>
