26e4b7e380d0fe4c9a3c2685180d99ef20641de2
Praveen Bisht <prvnbist@gmail.com>
2019-11-06 15:05:23 +0530

    refactor(styles)

--- src/pages/basicInfo/RegisterEmail.jsx
+++ src/pages/basicInfo/RegisterEmail.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import styled from 'styled-components'
+
+import { Wrapper, Field, Label, Form } from './Styles'
 
 const RegisterEmail = () => (
 	<Wrapper>
-		<h4>Register with your work address.</h4>
+		<h4>Register with your work address</h4>
 		<Form>
 			<Field>
 				<input type="text" id="email" required />
@@ -14,11 +15,28 @@ const RegisterEmail = () => (
 				<Label htmlFor="password">Password</Label>
 			</Field>
 			<div>
-				<CheckBox type="checkbox" id="terms" />
+				<input type="checkbox" id="terms" />
 				<label htmlFor="terms" id="terms__label">
-					I agree to <a href="#">terms & conditions</a>,{' '}
-					<a href="#">cancellation policy</a> and{' '}
-					<a href="#">privacy policy</a>.
+					I agree to{' '}
+					<a
+						href="/terms-and-conditions"
+						aria-labelledby="read about our terms and conditions"
+						title="Read about terms and conditions">
+						terms & conditions,
+					</a>{' '}
+					<a
+						href="/cancellation-policy"
+						aria-labelledby="read about our cancellation policy"
+						title="Read about our cancellation policy">
+						cancellation policy
+					</a>{' '}
+					and{' '}
+					<a
+						href="/privacy-policy"
+						aria-labelledby="read about privacy policy"
+						title="Read about privacy policy">
+						privacy policy.
+					</a>
 				</label>
 			</div>
 		</Form>
@@ -26,58 +44,3 @@ const RegisterEmail = () => (
 )
 
 export default RegisterEmail
-
-const Wrapper = styled.div`
-	h4 {
-		font-size: 16px;
-		font-weight: 400;
-		color: #555b6e;
-		margin-bottom: 24px;
-	}
-`
-
-const Form = styled.form`
-	width: 240px;
-	#terms__label {
-		font-size: 14px;
-		color: #888d9d;
-		margin-left: 8px;
-		a {
-			text-decoration: none;
-			color: #888d9d;
-			font-weight: 500;
-			&:hover {
-				color: #6e7382;
-			}
-		}
-	}
-`
-
-const Label = styled.label`
-	display: block;
-	font-size: 14px;
-	color: #969696;
-	transition: 0.3s ease-in-out;
-	transform: translateY(-32px);
-`
-
-const Field = styled.div`
-	input {
-		width: 240px;
-		height: 48px;
-		border: none;
-		border-bottom: 2px solid #e1e1e1;
-		&:focus {
-			outline: transparent;
-			border-bottom: 2px solid #04a777;
-		}
-		&:focus,
-		&:valid {
-			& + label {
-				transform: translateY(-56px);
-			}
-		}
-	}
-`
-
-const CheckBox = styled.input``
