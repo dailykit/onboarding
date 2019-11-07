8a4f70341a71f9da494c753637805ab9e1b7de3a
Praveen Bisht <prvnbist@gmail.com>
2019-11-07 14:39:30 +0530

    remove(consoles)

--- src/state/reducers.js
+++ src/state/reducers.js
@@ -3,18 +3,14 @@ export const PREV_PAGE = 'PREV_PAGE'
 
 const reducers = (state, action) => {
 	switch (action.type) {
-		case NEXT_PAGE: {
-			console.log('next')
+		case NEXT_PAGE:
 			return {
 				step: state.step + 1
 			}
-		}
-		case PREV_PAGE: {
-			console.log('prev')
+		case PREV_PAGE:
 			return {
 				step: state.step === 0 ? 0 : state.step - 1
 			}
-		}
 		default:
 			return state
 	}
