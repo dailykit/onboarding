4ced8f338152ca7cb8093243deac9b9c10bfc700
Praveen Bisht <prvnbist@gmail.com>
2020-05-06 15:53:13 +0530

    feat: apollo client integration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
 
 import App from './App'
 import './global.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const client = new ApolloClient({
+	uri: process.env.REACT_APP_DAILYCLOAK_URL
+})
+
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById('root')
+)
