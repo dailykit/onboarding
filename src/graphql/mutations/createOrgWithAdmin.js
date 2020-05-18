84c1b8d8eb586168e2e273ac81193a9b7a8cb0b6
Praveen Bisht <prvnbist@gmail.com>
2020-05-18 16:37:07 +0530

    fix: unexpected variable

--- src/graphql/mutations/createOrgWithAdmin.js
+++ src/graphql/mutations/createOrgWithAdmin.js
@@ -3,6 +3,7 @@ import { gql } from 'apollo-boost'
 export const CREATE_ORG_WITH_ADMIN = gql`
 	mutation createOrganization(
 		$organizationName: String
+		$organizationUrl: String
 		$organizationAdmins: organization_organizationAdmin_arr_rel_insert_input
 	) {
 		createOrganization(
