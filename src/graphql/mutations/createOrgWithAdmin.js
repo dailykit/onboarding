4f479488abf4fc3041de27ccb7c46fb1e10a85a8
Praveen Bisht <prvnbist@gmail.com>
2020-06-24 22:23:56 +0530

    fix: missing field

--- src/graphql/mutations/createOrgWithAdmin.js
+++ src/graphql/mutations/createOrgWithAdmin.js
@@ -4,6 +4,7 @@ export const CREATE_ORG_WITH_ADMIN = gql`
 	mutation createOrganization(
 		$organizationName: String
 		$organizationUrl: String
+		$timeZone: String
 		$organizationAdmins: organization_organizationAdmin_arr_rel_insert_input
 	) {
 		createOrganization(
